2d63073f8be8c34f4dc88c515c249e3f00f3d9d9
Michiko  Inoue <inouem888@gmail.com>
2024-10-10 12:33:48 +0000

    Add button test function

---
+++ scripts/tests/button.test.js
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+
+
+const buttonClick = require("../button");
+
+
+beforeEach(() => {
+  document.body.innerHTML = "<p id='par'></p>";
+});
+
+describe("DOM tests", () => {
+  test("expects p content to change", () => {
+    buttonClick();
+    expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+  });
+});
